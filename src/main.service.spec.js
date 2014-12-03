005aebcbbf809a042e871ea90a1bf61b40760780
franleplant <franleplant@gmail.com>
2014-12-03 15:39:36 -0300

    add one more assertion

--- src/main.service.spec.js
+++ src/main.service.spec.js
@@ -1,14 +1,14 @@
 describe('pctMoment.service', function () {
     'use strict';
 
-    beforeEach(module('pctMoment.service'));
 
     describe('when moment.js is present', function () {
+        beforeEach(module('pctMoment.service'));
+
         it('should return a reference to window.moment', inject(function (moment) {
 
             expect(moment).toBeDefined();
+            expect(moment).toBe(window.moment);
         }))
-    })
-
-
+    });
 });
