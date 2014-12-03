4ed60625934cc4d5674349f4358df220e47338d0
franleplant <franleplant@gmail.com>
2014-12-03 16:19:14 -0300

    add more docs

--- src/main.module.js
+++ src/main.module.js
@@ -1,6 +1,18 @@
 (function (angular) {
     'use strict';
 
+    /*
+     * @ngdoc module
+     * @name pctMoment
+     * @description
+     *
+     * Wrapper for [Moment.js](http://momentjs.com) and potentially for
+     * [moment-timezone.js](http://momentjs.com/timezone).
+     *
+     * Checkout detailed docs here: {@link pctMoment.service.moment 'moment'}
+     *
+     *
+     */
     angular.module('pctMoment', [
         'pctMoment.service'
     ]);
