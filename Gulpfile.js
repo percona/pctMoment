40c38a8adf00e1549312ef54ada425e687f42c2a
franleplant <franleplant@gmail.com>
2014-12-04 10:03:25 -0300

    add jscs to the project

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var karma = require('karma').server;
+var jscs = require('gulp-jscs');
 
 /**
- * Run test once and exit
+ *   Run test once and exit
  */
-gulp.task('test', function (done) {
+gulp.task('test', function(done) {
     karma.start({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true
@@ -12,6 +13,34 @@ gulp.task('test', function (done) {
 });
 
 
-gulp.task('continuous', ['test']);
+/**
+ *
+ * Run jscs (Javascript Code Style checker)
+ * over all the js files.
+ *
+ * Checkout .jscsrc for config parameteres
+ *
+ */
+gulp.task('jscs', function() {
+    return gulp.src([
+            'src/**/*.js',
+            'Gulpfile.js',
+            'karma.conf.js'
+        ])
+        .pipe(jscs());
+});
+
+
+
+/*
+ *
+ * Continuous Integration service default task
+ *
+ *
+ */
+gulp.task('continuous', [
+        'test',
+        'jscs'
+    ]);
 
 gulp.task('default', []);
